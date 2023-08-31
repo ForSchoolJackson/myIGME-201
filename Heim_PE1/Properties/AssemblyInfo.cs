ec318d0d706fbbcc40ddb1087f77e6817b477d8c
ForSchoolJackson <143518528+ForSchoolJackson@users.noreply.github.com>
2023-08-31 13:41:48 -0400

    Add files via upload
    Hello World

---
+++ Heim_PE1/Properties/AssemblyInfo.cs
@@ -0,0 +1,48 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+<<<<<<< HEAD:GithubFirstCommit/Properties/AssemblyInfo.cs
+[assembly: AssemblyTitle("GithubFirstCommit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GithubFirstCommit")]
+=======
+[assembly: AssemblyTitle("Heim_PE1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Heim_PE1")]
+>>>>>>> Hello World:Heim_PE1/Properties/AssemblyInfo.cs
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+<<<<<<< HEAD:GithubFirstCommit/Properties/AssemblyInfo.cs
+[assembly: Guid("f19de00a-b8aa-4ba0-9c37-fe05e682a7c8")]
+=======
+[assembly: Guid("92bf9625-36d0-49be-a020-3c9944bf25a7")]
+>>>>>>> Hello World:Heim_PE1/Properties/AssemblyInfo.cs
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
