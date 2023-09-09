b121e2e09d28050d28ade8f9c97066ad6ed1bc1b
Jackson <jacksontheim@gmail.com>
2023-09-08 22:25:25 -0400

    idk what im doing

---
+++ PE4-part2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE4_part2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string favoriteFood = "spaghetti";
+            switch (favoriteFood)
+            {
+                case ("spaghetti"):
+                    Console.WriteLine("You seem to really like spaghetti");
+                    break;
+                case ("cake"):
+                    Console.WriteLine("Wrong. Spaghetti is the best food.");
+                    break;
+                default:
+                    Console.WriteLine("You should like spaghetti.");
+                    break;
+            }
+
+
+
+        }
+    }
+}
