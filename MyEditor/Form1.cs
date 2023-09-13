c1e2fe203950aa0e667fed495cd4f3a2b7857eff
jackson <jacksontheim@gmail.com>
2023-09-12 21:50:59 -0400

    done

---
+++ MyEditor/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.newToolStripMenuItem.Click += new EventHandler(NewToolStripMenuItem_Click);
+            this.openToolStripMenuItem.Click += new EventHandler(OpenToolStripMenuItem_Click);
+            this.saveToolStripMenuItem.Click += new EventHandler(SaveToolStripMenuItem_Click);
+            this.exitToolStripMenuItem.Click += new EventHandler(ExitToolStripMenuItem_Click);
+
+            this.copyToolStripMenuItem.Click += new EventHandler(CopyToolStripMenuItem_Click);
+            this.cutToolStripMenuItem.Click += new EventHandler(CutToolStripMenuItem_Click);
+            this.pasteToolStripMenuItem.Click += new EventHandler(PasteToolStripMenuItem_Click);
+
+            this.toolStrip.ItemClicked += new ToolStripItemClickedEventHandler(ToolStrip_ItemClicked);
+            this.Text = "MyEditor";
+
+        }
+
+        //new tool
+        private void NewToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox.Clear();
+            this.Text = "MyEditor";
+
+        }
+
+        //Open tool
+        private void OpenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                RichTextBoxStreamType richTextBoxStreamType = RichTextBoxStreamType.RichText;
+
+                if(openFileDialog.FileName.ToLower().Contains(".txt"))
+                {
+                    richTextBoxStreamType = RichTextBoxStreamType.PlainText;
+                }
+                richTextBox.LoadFile(openFileDialog.FileName, richTextBoxStreamType);
+
+                this.Text = "MyEditor (" + openFileDialog.FileName + ")";
+
+            }
+        }
+
+        //save tool
+        private void SaveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog.FileName = openFileDialog.FileName;
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                RichTextBoxStreamType richTextBoxStreamType = RichTextBoxStreamType.RichText;
+
+                if (saveFileDialog.FileName.ToLower().Contains(".txt"))
+                {
+                    richTextBoxStreamType = RichTextBoxStreamType.PlainText;
+                }
+                richTextBox.SaveFile(saveFileDialog.FileName, richTextBoxStreamType);
+
+                this.Text = "MyEditor (" + saveFileDialog.FileName + ")";
+            }
+        }
+
+        //exit tool
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //copy tool
+        private void CopyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox.Copy();
+        }
+
+        //cut tool
+        private void CutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox.Cut();
+        }
+
+        //paste tool
+        private void PasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox.Paste();
+        }
+
+        //differnt text modes
+        private void ToolStrip_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            FontStyle fontStyle = FontStyle.Regular;
+
+            ToolStripButton toolStripButton = null;
+
+            //bold
+            if(e.ClickedItem == this.boldToolStripButton)
+            {
+                fontStyle = FontStyle.Bold;
+                toolStripButton = this.boldToolStripButton;
+
+            }//italic
+            else if(e.ClickedItem == this.italicsToolStripButton)
+            {
+                fontStyle = FontStyle.Italic;
+                toolStripButton = this.italicsToolStripButton;
+            }//underline
+            else if (e.ClickedItem == this.underlineToolStripButton)
+            {
+                fontStyle = FontStyle.Underline;
+                toolStripButton = this.underlineToolStripButton;
+            }//color
+            else if (e.ClickedItem == this.colorToolStripButton)
+            {
+                if (colorDialog.ShowDialog() == DialogResult.OK)
+                {
+                    richTextBox.SelectionColor = colorDialog.Color;
+                    colorToolStripButton.BackColor = colorDialog.Color;
+                }
+            }
+
+            //font styles
+            if( fontStyle != FontStyle.Regular)
+            {
+                toolStripButton.Checked = !toolStripButton.Checked;
+
+                SetSelectionFont(fontStyle, toolStripButton.Checked);
+            }
+
+        }
+
+        //selecting font
+        private void SetSelectionFont(FontStyle fontStyle, bool bSet)
+        {
+            Font newFont = null;
+            Font selectionFont = null;
+
+
+            selectionFont = richTextBox.SelectionFont;
+            if(selectionFont == null)
+            {
+                selectionFont = richTextBox.Font;
+            }
+
+            if (bSet)
+            {
+                newFont = new Font(selectionFont, selectionFont.Style | fontStyle);
+            }
+            else
+            {
+                //underline = 4, Bold = 1, Italics + 2
+                //all  three is 111
+                //underline is 100
+                newFont = new Font(selectionFont, selectionFont.Style & ~fontStyle);
+            }
+
+            this.richTextBox.SelectionFont = newFont;
+
+        }
+
+
+    }
+}
