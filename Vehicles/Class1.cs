c5eb43dc134ebc8997d55414dc11f6ddacaab63a
jackson <jacksontheim@gmail.com>
2023-10-05 12:49:56 -0400

    thing

---
+++ Vehicles/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicles
+{
+    public abstract class Vehicle
+    {
+    }
+
+    public abstract class PassengerTrain: Vehicle
+    {
+
+    }
+
+    public abstract class Car: Vehicle
+    {
+
+    }
+}
