fcc1d7877566139441e7973bc6ff466344e7e893
jackson <jacksontheim@gmail.com>
2023-10-05 13:08:48 -0400

    lib

--- Vehicles/Class1.cs
+++ Vehicles/Class1.cs
@@ -6,17 +6,61 @@ using System.Threading.Tasks;
 
 namespace Vehicles
 {
+
+    public interface IPassengerCarrier
+    {
+     
+    }
+
+    public interface IHeavyLoadCarrier
+    {
+
+    }
+
     public abstract class Vehicle
     {
     }
 
-    public abstract class PassengerTrain: Vehicle
+    public abstract class Train : Vehicle
     {
 
     }
 
-    public abstract class Car: Vehicle
+    public abstract class Car : Vehicle
     {
 
     }
+
+    public class PassengerTrain : Train, IPassengerCarrier
+    {
+
+    }
+
+    public class FreightTrain : Train
+    {
+
+    }
+
+    public class _424DoubleBougey : Train
+    {
+
+    }
+
+    public class Compact : Car , IPassengerCarrier
+    {
+
+    }
+
+    public class Pickup : Car , IHeavyLoadCarrier,IPassengerCarrier
+    {
+
+    }
+
+    public class SUV : Car, IPassengerCarrier
+    {
+
+    }
+
+ 
+
 }
