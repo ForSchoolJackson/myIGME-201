bf0495c0ae27457f46942633fa64508d4c4c3022
jackson <jacksontheim@gmail.com>
2023-10-05 13:14:08 -0400

    lib

--- Vehicles/Class1.cs
+++ Vehicles/Class1.cs
@@ -4,63 +4,78 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//vehicle library
+//jackson heim
 namespace Vehicles
 {
 
+    //passanger carry interface
     public interface IPassengerCarrier
     {
-     
+        
+
     }
 
+    //heavy load carry interface
     public interface IHeavyLoadCarrier
     {
 
     }
 
+    //parent vehicle class
     public abstract class Vehicle
     {
     }
 
+    //parent train class, child of vehicle
     public abstract class Train : Vehicle
     {
+        public virtual void LoadPassenger() { }
 
     }
 
+    //parent car class, child of vehicle
     public abstract class Car : Vehicle
     {
 
     }
 
+    //child passenger train class
     public class PassengerTrain : Train, IPassengerCarrier
     {
 
     }
 
+    //child freight train class
     public class FreightTrain : Train, IHeavyLoadCarrier
     {
 
     }
 
+    //child 424 double bougy class
     public class _424DoubleBougey : Train
     {
 
     }
 
+    //child compact class
     public class Compact : Car , IPassengerCarrier
     {
 
     }
 
+    //child pickup classs
     public class Pickup : Car , IHeavyLoadCarrier,IPassengerCarrier
     {
 
     }
 
+    //child suv class
     public class SUV : Car, IPassengerCarrier
     {
 
     }
 
- 
+
 
 }
