3bf268adcc178ab353fe5811b1277e85d97aa968
jackson <jacksontheim@gmail.com>
2023-10-12 13:30:24 -0400

    here

--- PE12-3/Program.cs
+++ PE12-3/Program.cs
@@ -23,17 +23,28 @@ namespace PE12_3
             }
         }
 
-
-
-
     }
 
-    //child class
+    //child class fo MyCLass
     class MyDerivedClass : MyClass
     {
+        //ovveride the old GetString from parent to change it 
         public override string GetString()
         {
             return base.GetString() + " (output from derived class)";
+        }
+    }
+
+    //program class
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDerivedClass myDeriv = new MyDerivedClass();
+
+            Console.WriteLine(myDeriv.GetString());
+
+
         }
     }
 
