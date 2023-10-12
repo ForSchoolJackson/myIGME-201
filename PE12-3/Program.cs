d91ac0da525fc5e885ee62939853c5f16ba9aee5
jackson <jacksontheim@gmail.com>
2023-10-12 13:23:29 -0400

    done

---
+++ PE12-3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE12_3
+{
+    //parent class
+    public class MyClass
+    {
+        private string myString;
+
+        public virtual string GetString()
+        {
+            return myString;
+        }
+        public string MyString
+        {
+            set
+            {
+                myString = value;
+            }
+        }
+
+
+
+
+    }
+
+    //child class
+    class MyDerivedClass : MyClass
+    {
+        public override string GetString()
+        {
+            return base.GetString() + " (output from derived class)";
+        }
+    }
+
+}
+
