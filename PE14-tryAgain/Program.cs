dd99f53bc8fa40753d8ce2879592765e3d118f86
jackson <jacksontheim@gmail.com>
2023-10-17 13:13:35 -0400

    uh

---
+++ PE14-tryAgain/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE14
+{
+    public interface IClass
+    {
+        void MyString();
+    }
+
+    public class ClassOne : IClass
+    {
+        public void MyString()
+        {
+            Console.WriteLine("String for class one.");
+        }
+    }
+
+    public class ClassTwo : IClass
+    {
+        public void MyString()
+        {
+            Console.WriteLine("String for class two.");
+        }
+    }
+
+
+    //program class
+    internal class Program
+    {
+        //main
+        static void Main(string[] args)
+        {
+            //call on the MyMethod method for each
+            public static void MyMethod(object myObject)
+            {
+                myObject.IClass.MyString();
+            }
+
+            //create object of both classes
+            ClassOne objOne = new ClassOne();
+            ClassTwo objTwo = new ClassTwo();
+
+
+            //call my method
+            MyMethod(objOne);
+            MyMethod(objTwo);
+
+
+        }
+    }
+}
