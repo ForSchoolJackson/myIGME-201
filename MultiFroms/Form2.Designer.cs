20a4c7f34e0c29fa461960aa6591de56fbbfdddd
jackson <jacksontheim@gmail.com>
2023-11-27 20:56:45 -0500

    i did it ;)

--- MultiFroms/Form2.Designer.cs
+++ MultiFroms/Form2.Designer.cs
@@ -93,6 +93,7 @@
             // 
             // GameForm
             // 
+            this.AcceptButton = this.guessButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(546, 216);
